e4a1e055eb93cf1e0f79a0a709da3eca753da403
lhays <lha2362g.uky.edu>
2022-03-18 15:45:01 -0400

    Add global style and clean up unused css and svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
-import './App.css';
+import styled from '@emotion/styled';
 
-const App = (): JSX.Element => <div>Hello Typescript</div>;
+const App = (): JSX.Element => {
+  const Header = styled('div')({
+    width: '100%',
+    height: '100px',
+    backgroundColor: 'black',
+    margin: '0',
+  });
+
+  const Body = styled('div')({
+    width: '100%',
+    height: '200px',
+    backgroundColor: 'white',
+    margin: '0',
+  });
+
+  return (
+    <div>
+      <Header />
+      <Body>Hello Typescript</Body>
+    </div>
+  );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Global, css } from '@emotion/react';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const globalStyle = css({
+  body: {
+    margin: '0px',
+    height: '100%',
+    fontFamily: `-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+    'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 
+    'Helvetica Neue', sans-serif`,
+    WebkitFontSmoothing: 'antialiased',
+    MozOsxFontSmoothing: 'grayscale',
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
+    <Global styles={globalStyle} />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
