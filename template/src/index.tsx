ef850667d8422bb6c25762d65e1294bfce8b7b10
Midival <lukehays32@gmail.com>
2022-03-30 22:01:38 -0400

    update index render for React 18

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Global, css, ThemeProvider } from '@emotion/react';
 import theme from 'theme';
 
@@ -27,14 +27,17 @@ const globalStyle = css({
   },
 });
 
-ReactDOM.render(
+const container = document.getElementById('root') as HTMLElement;
+
+const root = ReactDOM.createRoot(container);
+
+root.render(
   <React.StrictMode>
     <Global styles={globalStyle} />
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
